7f531f30b7f3e4372cbae09debf5b56b22c45bd1
Shin SeockJu <margron@naver.com>
2020-08-09 23:57:53 +0900

    주석 추가 및 tfidf.py 일부 수정

--- tfidf/sentencepiece_on_project.py
+++ tfidf/sentencepiece_on_project.py
@@ -1,6 +1,8 @@
 import os
 import sentencepiece as spm
 
+# sentencepiece model을 학습시킵니다.
+# lxml_OpenAPI 모듈에서 all_in_one함수를 실행시켰다는 전제하에 이용합니다.
 def train_sp(vocab_size, is_src=True):
     template = "--input={} \
                 --pad_id={} \
@@ -23,9 +25,12 @@ def train_sp(vocab_size, is_src=True):
                             1.0,
                             'bpe')
 
+    # 실제 학습이 시작되는 함수
     spm.SentencePieceTrainer.Train(config)
     
+# 이미 학습이 되었다는 가정하에 학습된 모델을 가져옵니다.
 def get_sentencepiece():
     src_sp = spm.SentencePieceProcessor()
+    # 현재 디렉토리에 학습된 모델이 있어야합니다.
     src_sp.Load("output.model")
     return src_sp
--- tfidf/tfidf.py
+++ tfidf/tfidf.py
@@ -1,54 +1,80 @@
 # scikit-learn 패키지를 이용하여 tfidf를 구현합니다.
 # scikit-learn 패키지를 이용하여 cosine similarity를 계산합니다.
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.metrics.pairwise import manhattan_distances
 import numpy as np
 
+# tfidf 모델을 로컬에 저장하기 위한 패키지
 import pickle
 
-def preprocessing(sentencepiece_model, data, stopwords, Print=False):
-    data_no_stopwords=[]
-    for sentence in data:
-        temp_X = sentencepiece_model.EncodeAsPieces(sentence)
 
-        temp_X=[word for word in temp_X if not word in stopwords] # 불용어 제거
-        data_no_stopwords.append(temp_X)
-
-    if (Print):
-        print('불용어 제거된 data는 다음과 같습니다')
-        print(data_no_stopwords)
-        
-    realdata=[]
-
-    for sentence in data_train :
-        test=' '.join(sentence).replace('▁', '')
-        realdata.append(test)
+# 학습된 sentencepiece model을 이용하여 data를 tfidf에 이용할 수 있도록 전처리 합니다.
+def preprocessing(sentencepiece_model, files : str[], stopwords, Print=False):
+    """
+    sentencepiece_model : 이미 학습된 sentencepiece_model 객체를 받는 parameter입니다.
     
+    files : tfidf에 이용할 file 리스트 입니다. 
+    type은 str입니다.
+    
+    stopwords : 불용언을 답고 있는 리스트 입니다.
+    type은 str[]입니다.
+    
+    Print : 중간 결과를 프린트 할지에 대한 parameter입니다.
+    기본값은 False입니다.
+    """
+    realdata=[]
+    for file in files:
+        temp_X = sentencepiece_model.EncodeAsPieces(file)
+        
+        temp_X = [word for word in temp_X if not word in stopwords]
+        file_data=''
+        
+        for word in temp_X:
+            file_data.join(word)
+            
+        realdata.append(file_data.replace('_', ''))
+        
     if (Print):
         print('실제 사용될 data는 다음과 같습니다')
-        print(realdata)
+        for file_data in realdata:
+            print(file_data[:20] + '.....')
 
     return realdata
 
+# tfidf 모델을 학습시킵니다.
 def train(data):
+    """
+    data : 문서의 리스트로 이루어져있어야 합니다.
+    """
     tfidfv = TfidfVectorizer().fit(data)
     return tfidfv
 
+# tfidf 분석을 실시합니다.
 def do(modoel, data):
+    """
+    model : tfidf 분석을 실행할 모델
+    
+    data : tfidf 분석을 실행할 데이터
+    type은 str, str[]입니다.
+    """
     result = model.transform(data).toarray()
     return result
     
+# pickle 패키지를 이용하여 tfidf모델을 현재 로컬폴더에 저장합니다.
 def save(model, name):
     with open(name, 'wb') as file:
         pickle.dump(model, file)
         
+# pickle 패키지를 이용하여 tfidf모델을 현재 로컬폴더에서 가져옵니다.
+# 이름이 같다면 무조건 물러오기 때문에 이 함수는 불러오는 모듈이 tfidf모델인지 체크하지 않습니다.
 def load(name):
     with open(name, 'wb') as file:
         return pickle.load(file)
 
+    
+# region start 유사도 분석
 def cos_similarity(data1, data2, Print=False):
     # 코사인 유사도를 구합니다.
     similarity = cosine_similarity(data1, data2)
@@ -79,15 +105,21 @@ def manhattan_similarity(data1, data2, Print=False):
     similarity = manhattan_distances(norm_data1, norm_data2)
     if (Print):
         print('data 1, data 2 Manhattan 유사도: {0:.3f}'.format(similarity))
+# region end 유사도 분석        
 
+
+# 특정 벡터를 정규화합니다.
+# 본 프로젝트에서는 유사도 분석시의 결과를 0~1의 범위로 제한시키기 위해 tfidf결과값에 정규화를 취해준뒤 유사도를 분석합니다.
 def __normalize(v):
 	norm = np.sum(v)
     return v / norm 
 
+# tfidf를 이용하여 본프로젝트의 DB속 판례 데이터와 input으로 받은 data의 유사도를 전체적으로 분석하여 가장 높은 유사도를 보이는 top10을 골라줍니다.
 def top10(data, method, Print=False):
     # DB에서 받아와야됨
-    case_data_metrics
-    # case_data_metrics = 
+    # 각각 판례의 tfidf분석결과의 리스트가 각각의 인덱스에 들어가므로 결론적으로 2차원 배열이 된다.
+    case_data_metrics[]
+    # case_data_metrics.append(DB_data) 
 
     similarity = 0
     if (method == 'set'):
@@ -98,3 +130,5 @@ def top10(data, method, Print=False):
         similarity = manhattan_similarity(case_data_metrics, data, Print)
     elif (methoc == 'cos'):
         similarity = cos_similarity(case_data_metrics, data, Print)
+
+    # Todo
