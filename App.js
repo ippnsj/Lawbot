b6cee9318c75285f97a291c1e644f8a82479e5f7
unknown <ippnsj@gmail.com>
2020-10-14 23:28:42 +0900

    caseview merged to main frontmobile

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import WritePettition from "./app/screens/WritePettition";
 import SimilarCaseAnalysis from "./app/screens/SimilarCaseAnalysis";
 import TerminologyButton from "./app/screens/TerminologyButton";
 import TerminologyExplanation from "./app/screens/TerminologyExplanation";
+import CaseBefore from "./app/screens/CaseBefore";
+import CaseView from "./app/screens/CaseView";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -14,13 +16,15 @@ export default class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="WelcomeScreen">
+        <Stack.Navigator initialRouteName="CaseBefore">
           <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{ headerShown: false }} />
           <Stack.Screen name="Enrollment" component={Enrollment} options={{ headerShown: false }} />
           <Stack.Screen name="WritePettition" component={WritePettition} options={{ headerShown: false }} />
           <Stack.Screen name="SimilarCaseAnalysis" component={SimilarCaseAnalysis} options={{ headerShown: false }} />
           <Stack.Screen name="TerminologyButton" component={TerminologyButton} options={{ headerShown: false }} />
           <Stack.Screen name="TerminologyExplanation" component={TerminologyExplanation} options={{ headerShown: false }} />
+          <Stack.Screen name="CaseBefore" component={CaseBefore} options={{ headerShown: false }} />
+          <Stack.Screen name="CaseView" component={CaseView} options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     )
